6ba7b8436b31465cb1f395dcab551c2fcf7db21f
Crec0 <83436716+Crec0@users.noreply.github.com>
2022-01-09 22:27:21 -0800

    refactor: async fetchBlockList fix: fabric loader version mismatch crash

--- src/main/java/com/adryd/chatlagfix/mixin/MixinYggdrasilSocialInteractionsService_chatLagFix.java
+++ src/main/java/com/adryd/chatlagfix/mixin/MixinYggdrasilSocialInteractionsService_chatLagFix.java
@@ -1,69 +1,27 @@
 package com.adryd.chatlagfix.mixin;
 
-import com.adryd.chatlagfix.ChatLagFixMod;
-import com.mojang.authlib.exceptions.AuthenticationException;
-import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
 import com.mojang.authlib.yggdrasil.YggdrasilSocialInteractionsService;
-import com.mojang.authlib.yggdrasil.response.BlockListResponse;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.net.URL;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
-import static java.util.Collections.emptySet;
-
 @Mixin(value = YggdrasilSocialInteractionsService.class, remap = false)
 public abstract class MixinYggdrasilSocialInteractionsService_chatLagFix {
-
-    @Final
-    @Shadow
-    private URL routeBlocklist;
-
     @Shadow
     private Set<UUID> blockList;
 
-
     @Shadow
-    @Final
-    private YggdrasilAuthenticationService authenticationService;
+    protected abstract Set<UUID> fetchBlockList();
 
-    @Shadow
-    @Final
-    private String accessToken;
-
-    @Inject(method = "fetchBlockList", at = @At("HEAD"), cancellable = true)
-    private void safeForceFetchBlockList(CallbackInfoReturnable<Set<UUID>> cir) {
-        ChatLagFixMod.LOGGER.debug("YggdrasilUserApiService#forceFetchBlockList(): Fetching block list");
-        cir.setReturnValue(emptySet());
-        // Don't fetch block list multiple times
-        if (this.blockList != null) {
-            cir.setReturnValue(this.blockList);
-            return;
-        }
-        // Return an empty set immediately and update the list once the request has finished
-        // This should only create one thread
-        new Thread(() -> {
-            try {
-                final BlockListResponse response = ((IMixinYggdrasilAuthenticationService_makeRequestInvoker) authenticationService).invokeMakeRequest(routeBlocklist, null, BlockListResponse.class, "Bearer " + accessToken);
-                if (response == null) {
-                    ChatLagFixMod.LOGGER.debug("YggdrasilUserApiService#fetchBlockList(): Block list fetch was empty");
-                    this.blockList = emptySet();
-                    return;
-                }
-                ChatLagFixMod.LOGGER.debug("YggdrasilUserApiService#fetchBlockList(): Block list fetch was successful");
-                this.blockList = response.getBlockedProfiles();
-            } catch (AuthenticationException e) {
-                ChatLagFixMod.LOGGER.debug("YggdrasilUserApiService#fetchBlockList(): Block list fetch failed");
-            }
-        }).start();
-        cir.setReturnValue(Collections.emptySet());
+    @Redirect(method = "isBlockedPlayer", at = @At(value = "INVOKE", target = "Lcom/mojang/authlib/yggdrasil/YggdrasilSocialInteractionsService;fetchBlockList()Ljava/util/Set;"))
+    private Set<UUID> asyncFetchBlockList(YggdrasilSocialInteractionsService service) {
+        CompletableFuture.runAsync(() -> this.blockList.addAll(fetchBlockList()));
+        return new HashSet<>();
     }
 }
